d832a3c93bdacbc583669f42fc0a50e1d39418ed
vivek chauhan <vivekchauhan@viveks-MacBook-Pro.local>
2021-04-18 01:32:37 +0400

    add the route in file

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,4 +5,6 @@ module.exports = function(app){
     app.get('/user', function (req, res) {
         res.send('User Dashboard Page');
     });
+
+    app.get('/info', (req, res) => res.send('User Info'));
 }
