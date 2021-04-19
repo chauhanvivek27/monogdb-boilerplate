c013a840b818657439cd9b85e1b666df728d4224
vivek chauhan <vivekchauhan@viveks-MacBook-Pro.local>
2021-04-19 06:59:36 +0400

    adding custom response

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,5 @@
 const user = require('../api/user');
+const { success , error } = require('../helpers/app-reposne');
 module.exports = function(app){
     app.get('/user', async function (req, res) {
         const post = await user();
