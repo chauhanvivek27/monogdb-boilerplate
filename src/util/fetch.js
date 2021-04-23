9d12689e9a81033aad9192ebc31be0656c4943e3
vivek chauhan <vivekchauhan@viveks-MacBook-Pro.local>
2021-04-24 01:15:10 +0400

    remove async await

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -6,11 +6,11 @@ const LOG_SERVICE_RESPONSE_BODY = process.env.LOG_SERVICE_RESPONSE_BODY === 'tru
 
 //const json = await fetch(url, {body, method, headers}).then(res=> res.json())
 
-module.exports = async (url, opts) => {
+module.exports =  (url, opts) => {
   const responseStart = Date.now()
   logger.info(`Request URL ${url}`, LOG_SERVICE_REQUEST && opts || '')
 
-  const response = await fetch(url, opts)
+  const response =  fetch(url, opts)
   .then(res => {
     const responseTime = Date.now() - responseStart
     logger.info(`Response from URL ${url} received in ${responseTime} ms`, LOG_SERVICE_RESPONSE_BODY && response || '');
